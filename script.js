2dee5d5656ca8804b02f7346a5ea8f53393d23ca
gbura <rage6999@wp.pl>
2023-10-03 15:30:59 +0200

    added security checking whether the first character is not an operator

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let buttons = document.querySelectorAll('button')
 
 let result = ''
 
+const numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 buttons.forEach(el => {
 	el.addEventListener('click', e => {
-		if (e.target.innerText == '=') {
+		if (inputBox.value.length == 0 && !numArr.includes(e.target.innerText)) {
+			return
+		} else if (e.target.innerText == '=') {
 			result = String(eval(result))
 			inputBox.value = result
 		} else if (e.target.innerText == 'AC') {
